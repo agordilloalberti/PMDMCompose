58e899fb6690fed262d9c9dc07da638cde97679a
Alvarogogal <agorsua0610@g.educaand.es>
2024-11-20 20:31:55 +0100

    .

--- app/src/main/java/com/pmdmcompose/screens/Chat.kt
+++ app/src/main/java/com/pmdmcompose/screens/Chat.kt
@@ -2,15 +2,11 @@ package com.pmdmcompose.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -18,7 +14,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.pmdmcompose.R
-import com.pmdmcompose.navegacion.AppScreen
 
 @Composable
 fun Chat(navControlador: NavController,modifier: Modifier) {
